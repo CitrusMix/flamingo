709bb5e5f70107aa16abfbb6350498c3bd4e6394
citrus <klaudieczek@vp.pl>
2017-03-04 14:22:45 +0100

    Added ApplicationConfiguration and AppInitializer

---
+++ pl-spring-mvc/src/main/java/spring/mvc/config/ApplicationConfiguration.java
@@ -0,0 +1,30 @@
+package spring.mvc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan(basePackages="pl.spring.mvc")
+@EnableWebMvc
+public class ApplicationConfiguration extends WebMvcConfigurerAdapter {
+	
+	@Bean
+	public InternalResourceViewResolver viewResolver(){
+		InternalResourceViewResolver view = new InternalResourceViewResolver();
+		view.setPrefix("/WEB-INF/views/");
+		view.setSuffix(".jsp");
+		return view;
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+	
+
+}
--- pl-spring-mvc/src/main/java/thth/Rgvggg.java
+++
@@ -1,5 +0,0 @@
-package thth;
-
-public class Rgvggg {
-
-}
